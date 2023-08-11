5d50132d78f2bd71e10f144d19fde71f651baeae
jsmart-tech <94623920+jsmart-tech@users.noreply.github.com>
2023-08-11 13:57:07 +0100

    Create index.py

---
+++ index.py
@@ -0,0 +1,60 @@
+def crypted(statement):
+     encryption =""
+     for element in statement:
+          if element in "Aa":
+               encryption= encryption +"1"
+          elif element in "Bb":
+               encryption= encryption +"!"
+          elif element in "Cc":
+               encryption= encryption +"*"
+          elif element in "Dd":
+               encryption= encryption +"@"
+          elif element in "Ee":
+               encryption= encryption +")"
+          elif element in "Ff":
+               encryption= encryption +")"
+          elif element in "Gg":
+               encryption= encryption +"="
+          elif element in "Hh":
+               encryption= encryption +">"
+          elif element in "Ii":
+               encryption= encryption +","
+          elif element in "Jj":
+               encryption= encryption +"<"
+          elif element in "Kk":
+               encryption= encryption +"-"
+          elif element in "Ll":
+               encryption= encryption +"?"
+          elif element in "Mm":
+               encryption= encryption +"-"
+          elif element in "Nn":
+               encryption= encryption +"-"
+          elif element in "Oo":
+               encryption= encryption +"&"
+          elif element in "Pp":
+               encryption= encryption +"]"
+          elif element in "Qq":
+               encryption= encryption +"."
+          elif element in "Rr":
+               encryption= encryption +"^"
+          elif element in "Ss":
+               encryption= encryption +"~"
+          elif element in "Tt":
+               encryption= encryption +"<"
+          elif element in "Uu":
+               encryption= encryption +":"
+          elif element in "Vv":
+               encryption= encryption +";"
+          elif element in "Ww":
+               encryption= encryption +"]"
+          elif element in "Xx":
+               encryption= encryption +"%"
+          elif element in "Yy":
+               encryption= encryption +"<"
+          elif element in "Zz":
+               encryption= encryption +"G"
+          else :
+               encryption = encryption + element
+
+     return encryption
+print(crypted(input("enter the word to encrypt: ")))
